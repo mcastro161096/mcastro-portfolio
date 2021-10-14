1b18a93aa7c3282545bcb3d053bd3611c4f13dec
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-10-14 00:18:35 -0300

    Tranformando AppBarCollapse em function.

--- src/Components/Navegacao/AppBarCollapse.js
+++ src/Components/Navegacao/AppBarCollapse.js
@@ -23,26 +23,27 @@ const styles = theme => ({
   }
 });
 
-const AppBarCollapse = props => (
-  <div className={props.classes.root}>
-    <ButtonAppBarCollapse>
-      <MenuItem><Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }}  >Home</Link></MenuItem>
-      <MenuItem><Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none" }} >Cursos</Link></MenuItem>
-      <MenuItem><Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none" }} >Tecnologia</Link></MenuItem>
-      <MenuItem><Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none" }} >Projetos</Link></MenuItem>
-      <MenuItem><Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none" }} >Contato</Link></MenuItem>
-      <MenuItem><Button variant="outlined" component={Link} to="/" style={{ color: "#00b8ff", borderColor: "#00b8ff"}}>Resumo</Button></MenuItem>
-    </ButtonAppBarCollapse>
-    <div className={props.classes.buttonBar} id="appbar-collapse">
+function AppBarCollapse(props) {
+ return (
+    <div className={props.classes.root}>
+      <ButtonAppBarCollapse>
+        <MenuItem><Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }}  >Home</Link></MenuItem>
+        <MenuItem><Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none" }} >Cursos</Link></MenuItem>
+        <MenuItem><Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }} >Tecnologia</Link></MenuItem>
+        <MenuItem><Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }} >Projetos</Link></MenuItem>
+        <MenuItem><Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }} >Contato</Link></MenuItem>
+        <MenuItem><Button variant="outlined" component={Link} to="/" style={{ color: "#00b8ff", borderColor: "#00b8ff" }}>Resumo</Button></MenuItem>
+      </ButtonAppBarCollapse>
+      <div className={props.classes.buttonBar} id="appbar-collapse">
 
-      <Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }}  >Home</Link>
-      <Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Cursos</Link>
-      <Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Tecnologia</Link>
-      <Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Projetos</Link>
-      <Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Contato</Link>
-      <Button variant="outlined" component={Link} to="/" style={{ color: "#00b8ff", borderColor: "#00b8ff", marginLeft: 20 }}>Resumo</Button>
+        <Link to="/" style={{ color: "#ccd6f6", textDecoration: "none" }}  >Home</Link>
+        <Link to="/cursos" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Cursos</Link>
+        <Link to="/" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Tecnologia</Link>
+        <Link to="/" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Projetos</Link>
+        <Link to="/" style={{ color: "#ccd6f6", textDecoration: "none", marginLeft: 30 }} >Contato</Link>
+        <Button variant="outlined" component={Link} to="/" style={{ color: "#00b8ff", borderColor: "#00b8ff", marginLeft: 20 }}>Resumo</Button>
+      </div>
     </div>
-  </div>
-);
-
+  );
+}
 export default withStyles(styles)(AppBarCollapse);
