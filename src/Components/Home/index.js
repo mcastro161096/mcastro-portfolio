9e005082367901b186fe66ed8f36864ac755dac2
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-10-18 22:37:14 -0300

    DescricaoWrite passado para dentro do Home.

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import avatar from '../../static/images/avatar/avatar.jpg';
+import DescricaoWrite from '../DescricaoWrite';
 
 function Home() {
     return (
-        <div>
-            <Avatar alt="Matheus Castro" src={avatar} sx={{width:200, height:200, marginLeft:2, marginTop:10}}  />
-        </div>
+        <DescricaoWrite />
     );
 }
 
